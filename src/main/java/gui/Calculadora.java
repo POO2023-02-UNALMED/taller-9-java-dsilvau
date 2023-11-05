f288c4b5127c444160f29d89c805008aa424fbd5
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-11-04 20:57:16 -0500

    calculadora

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -1,25 +1,24 @@
 package gui;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Calculadora extends Application  {
-	
-	public static void main(String args[]){ 
-	      ***(args); 
-	} 
-	
+public class Calculadora extends Application {
+	public static void main(String args[]){
+		launch(args);
+	}
 	@Override
 	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
+		VBox vb = new VBox();
+		window.setTitle("Calculadora en java");
 		
 		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
+		window.show();
 	}
-	
 }
